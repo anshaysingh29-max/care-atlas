512745ddd7a03ccf33adc3d485b04eb8954cc8a4
anshaysingh29-max <anshaysingh29@gmail.com>
2026-08-24 12:27:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+
   images: {
+    unoptimized: true,
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' }
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      }
     ]
   }
 };
